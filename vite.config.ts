e6f2e029d630660ef19614189298143471e863f9
brandon-schabel <brandonschabel1995@gmail.com>
2025-06-19 22:53:07 -0700

    deploy updates

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [tsconfigPaths()],
+  // @ts-expect-error process is a nodejs global
+  base: process.env.NODE_ENV === 'production' ? '/td-engine/' : '/',
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
@@ -18,10 +20,10 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
       : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
